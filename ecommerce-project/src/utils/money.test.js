a311a11b506072c673cc502869dc84dea5471487
shaDC24 <shatabdi198dc@gmail.com>
2025-11-26 14:26:22 +0600

    Create unit test for format money

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,17 @@
+import { it,expect,describe } from 'vitest';
+import { formatMoney } from './money';
+
+
+describe('formatMoney',()=>{
+it('formats 0 cents as $0.00',()=>{
+    expect(formatMoney(0)).toBe('$0.00');
+
+});
+it('formats 1999 cents as $19.99',()=>{
+    expect(formatMoney(1999)).toBe('$19.99');
+});
+it('displays 2 decimals',()=>{
+    expect(formatMoney(1400)).toBe('$14.00');
+    expect(formatMoney(1000)).toBe('$10.00');
+});
+});
--- ecommerce-project/src/utils/time.js
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-export function getTimePercentage(estimatedDeliveryTimeMs,orderTimeMs) {
-    let totalDeliveryTimeMs=estimatedDeliveryTimeMs-orderTimeMs;
-    let timePassedMs=dayjs().valueOf()-orderTimeMs;
-    timePassedMs=0.3*totalDeliveryTimeMs; // For testing purpose only
-    let progressPercentage=Math.min((timePassedMs/totalDeliveryTimeMs)*100,100);
-    return progressPercentage;
-}
